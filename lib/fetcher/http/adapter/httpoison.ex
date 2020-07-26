f7471ad5a1646d34254c3e95e05f623b46c139d3
Gorka López de Torre <gorka@gorka.io>
2020-07-26 20:56:25 +0200

    Fix formatting

--- lib/fetcher/http/adapter/httpoison.ex
+++ lib/fetcher/http/adapter/httpoison.ex
@@ -6,9 +6,10 @@ defmodule Fetcher.Http.Adapter.Poison do
 
   @default_opts [follow_redirect: true, max_redirect: 3]
 
-  @spec get(String.t(), [key: any]) :: {:ok, Fetcher.Http.Client.body()} | {:error, term}
+  @spec get(String.t(), key: any) :: {:ok, Fetcher.Http.Client.body()} | {:error, term}
   def get(url, opts \\ []) do
     {headers, opts} = Keyword.pop(opts, :headers, [])
+
     case HTTPoison.get(url, headers, Keyword.merge(@default_opts, opts)) do
       {:ok, %{status_code: status, body: body}} when status >= 200 and status < 300 -> {:ok, body}
       {:ok, %{status_code: status}} -> {:error, status}
